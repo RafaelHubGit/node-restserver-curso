f38f26f0b002353f2dfc154f7c5b4a4001015602
GONZALO RAFAEL NAVA VARGAS <gonzalo.nava@senado.gob.mx>
2019-05-10 09:56:09 -0500

    Ya conecta con Atlas

--- server/config/config.js
+++ server/config/config.js
@@ -18,12 +18,13 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // ==========================
 let urlDB;
 
-// if( process.env.NODE_ENV === 'dev' ){
-    // urlDB = 'mongodb://localhost:27017/cafe';
+if( process.env.NODE_ENV === 'dev' ){
+    urlDB = 'mongodb://localhost:27017/cafe';
              
-// }else{
-    urlDB = 'mongodb+srv://MngAtls:EuuLWkp5taedRFAM@cluster0-vnraq.gcp.mongodb.net/cafe';
-// }
+}else{
+    // urlDB = 'mongodb+srv://MngAtls:EuuLWkp5taedRFAM@cluster0-vnraq.gcp.mongodb.net/cafe';
+    urlDB = 'mongodb+srv://MngAtls:EuuLWkp5taedRFAM@cluster0-vnraq.gcp.mongodb.net/cafe?retryWrites=true';
+}
 
 
 process.env.URLDB = urlDB;
